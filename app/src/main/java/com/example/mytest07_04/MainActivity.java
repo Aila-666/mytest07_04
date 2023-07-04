51ce027d04e203f1147a94de748ccc189f333245
Aila-666 <1727797813@qq.com>
2023-07-05 01:17:00 +0800

    [Test01][activity] add toast and button
    Signed-off-by: Aila-666 <1727797813@qq.com>

--- app/src/main/java/com/example/mytest07_04/MainActivity.java
+++ app/src/main/java/com/example/mytest07_04/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.mytest07_04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //test 2023/07/04 for first commit
         setContentView(R.layout.activity_main);
+        welcome();
+        Button button = findViewById(R.id.bt_android);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"button",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+    public void welcome(){
+        Toast.makeText(this, "欢迎来到我的世界", Toast.LENGTH_LONG).show();
+    }
+
+
 }
