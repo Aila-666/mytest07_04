66895898bc1c617b58e802e077d3c85e72ade32b
Aila-666 <1727797813@qq.com>
2023-07-04 23:36:39 +0800

    test 2023/07/04 for first commit

--- app/src/main/java/com/example/mytest07_04/MainActivity.java
+++ app/src/main/java/com/example/mytest07_04/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //test 2023/07/04 for first commit
         setContentView(R.layout.activity_main);
     }
 }
