96cc3652631d35480610e591ec4f4aca942f6cd4
Aila-666 <1727797813@qq.com>
2023-07-07 00:42:49 +0800

    [study][TextView] 2023/07/06
    Signed-off-by: Aila-666 <1727797813@qq.com>

--- app/src/main/java/com/example/mytest07_04/MainActivity.java
+++ app/src/main/java/com/example/mytest07_04/MainActivity.java
@@ -3,29 +3,21 @@ package com.example.mytest07_04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //test 2023/07/04 for first commit
         setContentView(R.layout.activity_main);
-        welcome();
-        Button button = findViewById(R.id.bt_android);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this,"button",Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-    public void welcome(){
-        Toast.makeText(this, "欢迎来到我的世界", Toast.LENGTH_LONG).show();
-    }
 
-
+        TextView tv_one = findViewById(R.id.tv_one);
+        tv_one.setText("xie");
+    }
 }
