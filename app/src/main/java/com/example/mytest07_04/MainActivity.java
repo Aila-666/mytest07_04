1d0e820d01d98075fc07e58e2f425affa3a6db26
Aila-666 <1727797813@qq.com>
2023-07-15 00:25:57 +0800

    [study][TextView] 2023/07/14
    Signed-off-by: Aila-666 <1727797813@qq.com>

--- app/src/main/java/com/example/mytest07_04/MainActivity.java
+++ app/src/main/java/com/example/mytest07_04/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv_one = findViewById(R.id.tv_one);
-        tv_one.setText("xie");
+//        TextView tv_one = findViewById(R.id.tv_one);
+//        tv_one.setText("Hi, i");
     }
 }
